3ea51a1e877f96e85f992e61798d0ca8a7f3ca74
edissonchamorroc <john.chamorro@udea.edu.co>
2023-03-10 09:31:02 -0500

    se configura proyecto debido a fallo con jpa

--- src/main/java/com/springboot/apisesiones/ApiSesionesApplication.java
+++ src/main/java/com/springboot/apisesiones/ApiSesionesApplication.java
@@ -2,12 +2,15 @@ package com.springboot.apisesiones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
+
 
 @SpringBootApplication
 public class ApiSesionesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiSesionesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSesionesApplication.class, args);
+    }
 
 }
